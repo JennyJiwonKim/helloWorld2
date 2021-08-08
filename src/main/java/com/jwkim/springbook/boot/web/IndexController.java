c9fd007dd514fe873cf35ad711b40ff5006d6f84
jwkim1215 <jwk1215@kccworld.net>
2021-08-09 00:26:41 +0900

    study 5th

--- src/main/java/com/jwkim/springbook/boot/web/IndexController.java
+++ src/main/java/com/jwkim/springbook/boot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.jwkim.springbook.boot.web;
 
+import com.jwkim.springbook.boot.config.auth.LoginUser;
+import com.jwkim.springbook.boot.config.auth.dto.SessionUser;
 import com.jwkim.springbook.boot.service.posts.PostsService;
 import com.jwkim.springbook.boot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
